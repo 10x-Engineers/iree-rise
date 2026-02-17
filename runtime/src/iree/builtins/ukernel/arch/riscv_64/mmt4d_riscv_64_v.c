04297237e28d26193017d8e75f1a31a075904c74
adeel10x <adeel.ahmad@10xengineers.ai>
2026-02-17 20:23:34 +0500

    Add MMT4D ukernel for s8xs8->s32 types for riscv_64 target
    Signed-off-by: adeel10x <adeel.ahmad@10xengineers.ai>

--- runtime/src/iree/builtins/ukernel/arch/riscv_64/mmt4d_riscv_64_v.c
+++ runtime/src/iree/builtins/ukernel/arch/riscv_64/mmt4d_riscv_64_v.c
@@ -133,3 +133,148 @@ IREE_UK_MMT4D_TILE_FUNC_IMPL_FOR_M0(
 IREE_UK_MMT4D_TILE_FUNC_IMPL_FOR_M0(
     iree_uk_mmt4d_tile_f32f32f32_1xXXx1_to_7xXXx1_riscv_64_v,
     iree_uk_mmt4d_tile_f32f32f32_7xXXx1_riscv_64_v, 7)
+
+IREE_UK_ATTRIBUTE_ALWAYS_INLINE static inline void
+iree_uk_mmt4d_tile_s8s8s32_1xXXx1_to_7xXXx1_riscv_64_v(
+    void* IREE_UK_RESTRICT out_tile, const void* IREE_UK_RESTRICT lhs_panel,
+    const void* IREE_UK_RESTRICT rhs_panel,
+    const iree_uk_mmt4d_params_t* params, int M0) {
+  IREE_UK_ASSERT(M0 >= 1 && M0 <= 7);
+
+  const iree_uk_int8_t* IREE_UK_RESTRICT lhs_ptr = lhs_panel;
+  const iree_uk_int8_t* IREE_UK_RESTRICT rhs_ptr = rhs_panel;
+  iree_uk_int32_t* IREE_UK_RESTRICT out_ptr = out_tile;
+
+  vint32m4_t acc0, acc1, acc2, acc3, acc4, acc5, acc6;
+  int N0 = params->N0;
+  size_t vl = N0;
+
+  if (M0 == 1) {
+    if (params->flags & IREE_UK_FLAG_MMT4D_ACCUMULATE) {
+      acc0 = __riscv_vle32_v_i32m4(out_ptr, vl);
+    } else {
+      acc0 = __riscv_vmv_v_x_i32m4(0.0, vl);
+    }
+    for (int k = 0; k < params->K; ++k) {
+      vint8m1_t rhs_s8 = __riscv_vle8_v_i8m1(rhs_ptr, vl);
+      vint16m2_t rhs_s16 = __riscv_vwcvt_x_x_v_i16m2(rhs_s8, vl);
+      rhs_ptr += N0;
+
+      iree_uk_int16_t lhs0 = (iree_uk_int16_t) * (lhs_ptr)++;
+
+      acc0 = __riscv_vwmacc_vx_i32m4(acc0, lhs0, rhs_s16, vl);
+    }
+    __riscv_vse32_v_i32m4(out_ptr, acc0, vl);
+  } else if (M0 == 2) {
+    if (params->flags & IREE_UK_FLAG_MMT4D_ACCUMULATE) {
+      acc0 = __riscv_vle32_v_i32m4(out_ptr, vl);
+      acc1 = __riscv_vle32_v_i32m4(out_ptr + N0, vl);
+    } else {
+      acc0 = __riscv_vmv_v_x_i32m4(0.0, vl);
+      acc1 = __riscv_vmv_v_x_i32m4(0.0, vl);
+    }
+    for (int k = 0; k < params->K; ++k) {
+      vint8m1_t rhs_s8 = __riscv_vle8_v_i8m1(rhs_ptr, vl);
+      vint16m2_t rhs_s16 = __riscv_vwcvt_x_x_v_i16m2(rhs_s8, vl);
+      rhs_ptr += N0;
+
+      iree_uk_int16_t lhs0 = (iree_uk_int16_t) * (lhs_ptr)++;
+      iree_uk_int16_t lhs1 = (iree_uk_int16_t) * (lhs_ptr)++;
+
+      acc0 = __riscv_vwmacc_vx_i32m4(acc0, lhs0, rhs_s16, vl);
+      acc1 = __riscv_vwmacc_vx_i32m4(acc1, lhs1, rhs_s16, vl);
+    }
+    __riscv_vse32_v_i32m4(out_ptr, acc0, vl);
+    __riscv_vse32_v_i32m4(out_ptr + N0, acc1, vl);
+  } else if (M0 == 4) {
+    if (params->flags & IREE_UK_FLAG_MMT4D_ACCUMULATE) {
+      acc0 = __riscv_vle32_v_i32m4(out_ptr, vl);
+      acc1 = __riscv_vle32_v_i32m4(out_ptr + N0, vl);
+      acc2 = __riscv_vle32_v_i32m4(out_ptr + N0 * 2, vl);
+      acc3 = __riscv_vle32_v_i32m4(out_ptr + N0 * 3, vl);
+    } else {
+      acc0 = __riscv_vmv_v_x_i32m4(0.0, vl);
+      acc1 = __riscv_vmv_v_x_i32m4(0.0, vl);
+      acc2 = __riscv_vmv_v_x_i32m4(0.0, vl);
+      acc3 = __riscv_vmv_v_x_i32m4(0.0, vl);
+    }
+    for (int k = 0; k < params->K; ++k) {
+      vint8m1_t rhs_s8 = __riscv_vle8_v_i8m1(rhs_ptr, vl);
+      vint16m2_t rhs_s16 = __riscv_vwcvt_x_x_v_i16m2(rhs_s8, vl);
+      rhs_ptr += N0;
+
+      iree_uk_int16_t lhs0 = (iree_uk_int16_t) * (lhs_ptr)++;
+      iree_uk_int16_t lhs1 = (iree_uk_int16_t) * (lhs_ptr)++;
+      iree_uk_int16_t lhs2 = (iree_uk_int16_t) * (lhs_ptr)++;
+      iree_uk_int16_t lhs3 = (iree_uk_int16_t) * (lhs_ptr)++;
+
+      acc0 = __riscv_vwmacc_vx_i32m4(acc0, lhs0, rhs_s16, vl);
+      acc1 = __riscv_vwmacc_vx_i32m4(acc1, lhs1, rhs_s16, vl);
+      acc2 = __riscv_vwmacc_vx_i32m4(acc2, lhs2, rhs_s16, vl);
+      acc3 = __riscv_vwmacc_vx_i32m4(acc3, lhs3, rhs_s16, vl);
+    }
+    __riscv_vse32_v_i32m4(out_ptr, acc0, vl);
+    __riscv_vse32_v_i32m4(out_ptr + N0, acc1, vl);
+    __riscv_vse32_v_i32m4(out_ptr + N0 * 2, acc2, vl);
+    __riscv_vse32_v_i32m4(out_ptr + N0 * 3, acc3, vl);
+  } else if (M0 == 7) {
+    if (params->flags & IREE_UK_FLAG_MMT4D_ACCUMULATE) {
+      acc0 = __riscv_vle32_v_i32m4(out_ptr, vl);
+      acc1 = __riscv_vle32_v_i32m4(out_ptr + N0, vl);
+      acc2 = __riscv_vle32_v_i32m4(out_ptr + N0 * 2, vl);
+      acc3 = __riscv_vle32_v_i32m4(out_ptr + N0 * 3, vl);
+      acc4 = __riscv_vle32_v_i32m4(out_ptr + N0 * 4, vl);
+      acc5 = __riscv_vle32_v_i32m4(out_ptr + N0 * 5, vl);
+      acc6 = __riscv_vle32_v_i32m4(out_ptr + N0 * 6, vl);
+    } else {
+      acc0 = __riscv_vmv_v_x_i32m4(0.0, vl);
+      acc1 = __riscv_vmv_v_x_i32m4(0.0, vl);
+      acc2 = __riscv_vmv_v_x_i32m4(0.0, vl);
+      acc3 = __riscv_vmv_v_x_i32m4(0.0, vl);
+      acc4 = __riscv_vmv_v_x_i32m4(0.0, vl);
+      acc5 = __riscv_vmv_v_x_i32m4(0.0, vl);
+      acc6 = __riscv_vmv_v_x_i32m4(0.0, vl);
+    }
+    for (int k = 0; k < params->K; ++k) {
+      vint8m1_t rhs_s8 = __riscv_vle8_v_i8m1(rhs_ptr, vl);
+      vint16m2_t rhs_s16 = __riscv_vwcvt_x_x_v_i16m2(rhs_s8, vl);
+      rhs_ptr += N0;
+
+      iree_uk_int16_t lhs0 = (iree_uk_int16_t) * (lhs_ptr)++;
+      iree_uk_int16_t lhs1 = (iree_uk_int16_t) * (lhs_ptr)++;
+      iree_uk_int16_t lhs2 = (iree_uk_int16_t) * (lhs_ptr)++;
+      iree_uk_int16_t lhs3 = (iree_uk_int16_t) * (lhs_ptr)++;
+      iree_uk_int16_t lhs4 = (iree_uk_int16_t) * (lhs_ptr)++;
+      iree_uk_int16_t lhs5 = (iree_uk_int16_t) * (lhs_ptr)++;
+      iree_uk_int16_t lhs6 = (iree_uk_int16_t) * (lhs_ptr)++;
+
+      acc0 = __riscv_vwmacc_vx_i32m4(acc0, lhs0, rhs_s16, vl);
+      acc1 = __riscv_vwmacc_vx_i32m4(acc1, lhs1, rhs_s16, vl);
+      acc2 = __riscv_vwmacc_vx_i32m4(acc2, lhs2, rhs_s16, vl);
+      acc3 = __riscv_vwmacc_vx_i32m4(acc3, lhs3, rhs_s16, vl);
+      acc4 = __riscv_vwmacc_vx_i32m4(acc4, lhs4, rhs_s16, vl);
+      acc5 = __riscv_vwmacc_vx_i32m4(acc5, lhs5, rhs_s16, vl);
+      acc6 = __riscv_vwmacc_vx_i32m4(acc6, lhs6, rhs_s16, vl);
+    }
+    __riscv_vse32_v_i32m4(out_ptr, acc0, vl);
+    __riscv_vse32_v_i32m4(out_ptr + N0, acc1, vl);
+    __riscv_vse32_v_i32m4(out_ptr + N0 * 2, acc2, vl);
+    __riscv_vse32_v_i32m4(out_ptr + N0 * 3, acc3, vl);
+    __riscv_vse32_v_i32m4(out_ptr + N0 * 4, acc4, vl);
+    __riscv_vse32_v_i32m4(out_ptr + N0 * 5, acc5, vl);
+    __riscv_vse32_v_i32m4(out_ptr + N0 * 6, acc6, vl);
+  }
+}
+
+IREE_UK_MMT4D_TILE_FUNC_IMPL_FOR_M0(
+    iree_uk_mmt4d_tile_s8s8s32_1xXXx1_to_7xXXx1_riscv_64_v,
+    iree_uk_mmt4d_tile_s8s8s32_1xXXx1_riscv_64_v, 1)
+IREE_UK_MMT4D_TILE_FUNC_IMPL_FOR_M0(
+    iree_uk_mmt4d_tile_s8s8s32_1xXXx1_to_7xXXx1_riscv_64_v,
+    iree_uk_mmt4d_tile_s8s8s32_2xXXx1_riscv_64_v, 2)
+IREE_UK_MMT4D_TILE_FUNC_IMPL_FOR_M0(
+    iree_uk_mmt4d_tile_s8s8s32_1xXXx1_to_7xXXx1_riscv_64_v,
+    iree_uk_mmt4d_tile_s8s8s32_4xXXx1_riscv_64_v, 4)
+IREE_UK_MMT4D_TILE_FUNC_IMPL_FOR_M0(
+    iree_uk_mmt4d_tile_s8s8s32_1xXXx1_to_7xXXx1_riscv_64_v,
+    iree_uk_mmt4d_tile_s8s8s32_7xXXx1_riscv_64_v, 7)
