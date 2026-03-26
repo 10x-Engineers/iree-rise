e460d0fd55b9847944a00dd4d42f932c1d6e77bf
Ahmad Tameem <113388789+Tameem-10xE@users.noreply.github.com>
2026-03-27 03:52:26 +0500

    Implement FP32 & FP16 tile function selection for RISC-V

--- runtime/src/iree/builtins/ukernel/arch/riscv_64/pack_riscv_64_entry_point.c
+++ runtime/src/iree/builtins/ukernel/arch/riscv_64/pack_riscv_64_entry_point.c
@@ -7,14 +7,53 @@
 #include "iree/builtins/ukernel/arch/riscv_64/common_riscv_64.h"
 #include "iree/builtins/ukernel/arch/riscv_64/pack_riscv_64_internal.h"
 
+static iree_uk_pack_tile_func_t iree_uk_pack_select_tile_func_riscv_64_7x1_x32(
+    const iree_uk_pack_params_t* params) {
+  bool transpose = params->flags & IREE_UK_FLAG_PACK_TRANSPOSE_INNER;
+  return transpose ? 0 : iree_uk_pack_tile_7x1_x32_riscv_64_direct;
+}
+
+static iree_uk_pack_tile_func_t
+iree_uk_pack_select_tile_func_riscv_64_7x32_x32(
+    const iree_uk_pack_params_t* params) {
+  bool transpose = params->flags & IREE_UK_FLAG_PACK_TRANSPOSE_INNER;
+  return transpose ? 0 : iree_uk_pack_tile_7x32_x32_riscv_64_direct;
+}
+
+static iree_uk_pack_tile_func_t
+iree_uk_pack_select_tile_func_riscv_64_32x1_x32(
+    const iree_uk_pack_params_t* params) {
+  bool transpose = params->flags & IREE_UK_FLAG_PACK_TRANSPOSE_INNER;
+  return transpose ? iree_uk_pack_tile_32x1_x32_riscv_64_transpose : 0;
+}
+
+static iree_uk_pack_tile_func_t iree_uk_pack_select_tile_func_riscv_64_7x1_x16(
+    const iree_uk_pack_params_t* params) {
+  bool transpose = params->flags & IREE_UK_FLAG_PACK_TRANSPOSE_INNER;
+  return transpose ? 0 : iree_uk_pack_tile_7x1_x16_riscv_64_direct;
+}
+
+static iree_uk_pack_tile_func_t
+iree_uk_pack_select_tile_func_riscv_64_7x32_x16(
+    const iree_uk_pack_params_t* params) {
+  bool transpose = params->flags & IREE_UK_FLAG_PACK_TRANSPOSE_INNER;
+  return transpose ? 0 : iree_uk_pack_tile_7x32_x16_riscv_64_direct;
+}
+
+static iree_uk_pack_tile_func_t
+iree_uk_pack_select_tile_func_riscv_64_32x1_x16(
+    const iree_uk_pack_params_t* params) {
+  bool transpose = params->flags & IREE_UK_FLAG_PACK_TRANSPOSE_INNER;
+  return transpose ? iree_uk_pack_tile_32x1_x16_riscv_64_transpose : 0;
+}
+
 iree_uk_pack_tile_func_t iree_uk_pack_select_tile_func_arch(
     const iree_uk_pack_params_t* params) {
-  // At the moment, as sum-reductions are not yet part of pack ops,
-  // no arithmetic whatsoever is being done here, so only the element type
-  // size matters, not the type itself.
+
   iree_uk_pack_type_t pack_type = iree_uk_pack_type(params->flags);
   int esize = iree_uk_type_size(iree_uk_pack_out_type(pack_type));
   bool transpose = params->flags & IREE_UK_FLAG_PACK_TRANSPOSE_INNER;
+
   if (esize == 1 && params->out_size2 != 7 && params->out_size3 == 1) {
     return transpose ? iree_uk_pack_tile_Xx1_x8_riscv_64_transpose : 0;
   } else if (esize == 1 && params->out_size2 == 7 && params->out_size3 != 1) {
@@ -22,5 +61,30 @@ iree_uk_pack_tile_func_t iree_uk_pack_select_tile_func_arch(
   } else if (esize == 1 && params->out_size2 == 7 && params->out_size3 == 1) {
     return transpose ? 0 : iree_uk_pack_tile_7x1_x8_riscv_64_direct;
   }
+
+  if (pack_type == iree_uk_pack_type_f16f16) {
+    if (params->out_size2 == 7 && params->out_size3 == 1) {
+      return iree_uk_pack_select_tile_func_riscv_64_7x1_x16(params);
+    }
+    if (params->out_size2 == 7 && params->out_size3 == 32) {
+      return iree_uk_pack_select_tile_func_riscv_64_7x32_x16(params);
+    }
+    if (params->out_size2 == 32 && params->out_size3 == 1) {
+      return iree_uk_pack_select_tile_func_riscv_64_32x1_x16(params);
+    }
+  }
+  
+  if (pack_type == iree_uk_pack_type_f32f32) {
+    if (params->out_size2 == 7 && params->out_size3 == 1) {
+      return iree_uk_pack_select_tile_func_riscv_64_7x1_x32(params);
+    }
+    if (params->out_size2 == 7 && params->out_size3 == 32) {
+      return iree_uk_pack_select_tile_func_riscv_64_7x32_x32(params);
+    }
+    if (params->out_size2 == 32 && params->out_size3 == 1) {
+      return iree_uk_pack_select_tile_func_riscv_64_32x1_x32(params);
+    }
+  }
+
   return 0;
 }
